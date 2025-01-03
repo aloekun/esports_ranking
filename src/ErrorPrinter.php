294a87e5c31af78ebd3d648426bd7039a73bf9c9
aloekun <56211144+aloekun@users.noreply.github.com>
2025-01-04 02:28:17 +0900

    public関数をクラスの上に移動

--- src/ErrorPrinter.php
+++ src/ErrorPrinter.php
@@ -3,6 +3,26 @@
 class ErrorPrinter
 {
     /**
+     * @param string $strInput
+     * @return string
+     */
+    public function printError($strInput)
+    {
+        // 数値比較の場合
+        if ($this->isDifferentNumber($strInput)) {
+            // フォントを変える数値を取得
+            $matches = $this->getDifferentNumber($strInput);
+            return 'Failed asserting that ' . $this->addStringColorRed($matches[1]) . ' is identical to ' . $this->addStringColorRed($matches[2]) . '.';
+        } elseif ($this->isDifferentString($strInput)) {
+            // フォントを変える文字列を取得
+            $matches = $this->getDifferentString($strInput);
+            return "Failed asserting that two strings are identical.\n--- Expected\n+++ Actual\n@@ @@\n-'" . $this->addStringColorRed($matches[1]) . "'\n+'" . $this->addStringColorRed($matches[2]) . "'";
+        }
+        // そのまま返す
+        return $strInput;
+    }
+
+    /**
      * 数値比較のエラーかどうか
      * @param string $strInput  入力文字列
      * @param string[] $matches マッチした文字列
@@ -76,26 +96,5 @@ class ErrorPrinter
     private function addStringColorRed($strInput)
     {
         return '<fg=red;options=bold>' . $strInput . '</>';
-    }
-
-
-    /**
-     * @param string $strInput
-     * @return string
-     */
-    public function printError($strInput)
-    {
-        // 数値比較の場合
-        if ($this->isDifferentNumber($strInput)) {
-            // フォントを変える数値を取得
-            $matches = $this->getDifferentNumber($strInput);
-            return 'Failed asserting that ' . $this->addStringColorRed($matches[1]) . ' is identical to ' . $this->addStringColorRed($matches[2]) . '.';
-        } elseif ($this->isDifferentString($strInput)) {
-            // フォントを変える文字列を取得
-            $matches = $this->getDifferentString($strInput);
-            return "Failed asserting that two strings are identical.\n--- Expected\n+++ Actual\n@@ @@\n-'" . $this->addStringColorRed($matches[1]) . "'\n+'" . $this->addStringColorRed($matches[2]) . "'";
-        }
-        // そのまま返す
-        return $strInput;
     }
 }
