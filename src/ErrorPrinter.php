f0529b6d3c04a97b3dfc875c3a3988fa5529e080
aloekun <56211144+aloekun@users.noreply.github.com>
2025-01-12 03:19:58 +0900

    PHPUnitの数値比較エラーを追加、クラスが見つからないケースを追加

--- src/ErrorPrinter.php
+++ src/ErrorPrinter.php
@@ -24,12 +24,23 @@ class ErrorPrinter
      */
     public function printError($strInput)
     {
+        // print("strInput: $strInput\n");
+
+        // $null = null;
+        // $null->testMethod();
         // 数値比較の場合
         if ($this->isDifferentNumber($strInput)) {
+            // print("isDifferentNumber\n");
             // フォントを変える数値を取得
             $matches = $this->getDifferentNumber($strInput);
             return 'Failed asserting that ' . $this->colorManager->addStringColorRed($matches[1]) . ' is identical to ' . $this->colorManager->addStringColorGreen($matches[2]) . '.';
+        } elseif ($this->isDifferentNumberPhpUnit($strInput)) {
+            // print("isDifferentNumberPhpUnit\n");
+            // フォントを変える数値を取得(PHPUnit)
+            $matches = $this->getDifferentNumberPhpUnit($strInput);
+            return 'Failed asserting that ' . $this->colorManager->addStringColorRed($matches[1]) . ' matches expected ' . $this->colorManager->addStringColorGreen($matches[2]) . '.';
         } elseif ($this->isDifferentString($strInput)) {
+            // print("isDifferentString\n");
             // フォントを変える文字列を取得
             $matches = $this->getDifferentString($strInput);
             // 改行コードが違っても対応するため、改行コードを取得
@@ -37,9 +48,15 @@ class ErrorPrinter
             // print("endOfLine: $endOfLine\n");
             return "Failed asserting that two strings are identical.$endOfLine--- Expected$endOfLine+++ Actual$endOfLine@@ @@$endOfLine-'" . $this->colorManager->addStringColorGreen($matches[1]) . "'$endOfLine+'" . $this->colorManager->addStringColorRed($matches[2]) . "'";
         } elseif ($this->isMissingClass($strInput)) {
+            // print("isMissingClass\n");
             $matches = $this->getMissingClass($strInput);
             return 'Class "' . $this->colorManager->addStringColorRed($matches[1]) . '" not found';
+        } elseif ($this->isMissingMethod($strInput)) {
+            // print("isMissingMethod\n");
+            $matches = $this->getMissingMethod($strInput);
+            return 'Call to undefined method ' . $this->colorManager->addStringColorRed($matches[1]);
         }
+        // print("そのまま返す\n");
         // そのまま返す
         return $strInput;
     }
@@ -51,7 +68,11 @@ class ErrorPrinter
      */
     public function isChangePrint($strInput)
     {
-        return $this->isDifferentNumber($strInput) || $this->isDifferentString($strInput) || $this->isMissingClass($strInput);
+        return $this->isDifferentNumber($strInput)
+            || $this->isDifferentNumberPhpUnit($strInput)
+            || $this->isDifferentString($strInput)
+            || $this->isMissingClass($strInput)
+            || $this->isMissingMethod($strInput);
     }
 
     /**
@@ -84,6 +105,39 @@ class ErrorPrinter
     {
         $matches = null;
         $this->checkDifferentNumber($strInput, $matches);
+        return $matches;
+    }
+
+    /**
+     * 数値比較のエラーかどうか(PHPUnit)
+     * @param string $strInput  入力文字列
+     * @param string[] $matches マッチした文字列
+     * @return bool マッチしたかどうか
+     */
+    private function checkDifferentNumberPhpUnit($strInput, &$matches = null)
+    {
+        return preg_match('/Failed asserting that (-?\d+) matches expected (-?\d+)./', $strInput, $matches);
+    }
+
+    /**
+     * 数値比較のエラーかどうか(PHPUnit)
+     * @param string $strInput
+     * @return bool
+     */
+    private function isDifferentNumberPhpUnit($strInput)
+    {
+        return $this->checkDifferentNumberPhpUnit($strInput);
+    }
+
+    /**
+     * フォントを変える数値を取得(PHPUnit)
+     * @param string $strInput
+     * @return string[]
+     */
+    private function getDifferentNumberPhpUnit($strInput)
+    {
+        $matches = null;
+        $this->checkDifferentNumberPhpUnit($strInput, $matches);
         return $matches;
     }
 
@@ -149,5 +203,22 @@ class ErrorPrinter
     private function checkMissingClass($strInput, &$matches = null)
     {
         return preg_match('/Class "(.*)" not found/', $strInput, $matches);
+    }
+
+    private function isMissingMethod($strInput)
+    {
+        return $this->checkMissingMethod($strInput);
+    }
+
+    private function getMissingMethod($strInput)
+    {
+        $matches = null;
+        $this->checkMissingMethod($strInput, $matches);
+        return $matches;
+    }
+
+    private function checkMissingMethod($strInput, &$matches = null)
+    {
+        return preg_match('/Call to undefined method (.*)/', $strInput, $matches);
     }
 }
--- tests/ErrorPrinterTest.php
+++ tests/ErrorPrinterTest.php
@@ -4,7 +4,7 @@ use ErrorPrintHelper\ErrorPrinter;
 use ErrorPrintHelper\ColorStringAscii;
 use ErrorPrintHelper\ColorStringHttp;
 
-test('文字列が返る(HTTP色変え, 数値比較1)', function () {
+test('文字列が返る(HTTP色変え, pest数値比較1)', function () {
     $colorManager = new ColorStringHttp();
     $sut = new ErrorPrinter($colorManager);
 
@@ -13,13 +13,31 @@ test('文字列が返る(HTTP色変え, 数値比較1)', function () {
     expect($result)->toBe('Failed asserting that <fg=red;options=bold>4</> is identical to <fg=green;options=bold>3</>.');
 });
 
-test('文字列が返る(HTTP色変え, 数値比較2)', function () {
+test('文字列が返る(HTTP色変え, pest数値比較2)', function () {
     $colorManager = new ColorStringHttp();
     $sut = new ErrorPrinter($colorManager);
 
     $result = $sut->printError('Failed asserting that 1 is identical to 10.');
 
     expect($result)->toBe('Failed asserting that <fg=red;options=bold>1</> is identical to <fg=green;options=bold>10</>.');
+});
+
+test('文字列が返る(HTTP色変え, pestクラス不明)', function () {
+    $colorManager = new ColorStringHttp();
+    $sut = new ErrorPrinter($colorManager);
+
+    $result = $sut->printError('Class "TestClass" not found');
+
+    expect($result)->toBe('Class "<fg=red;options=bold>TestClass</>" not found');
+});
+
+test('文字列が返る(HTTP色変え, phpunit数値比較)', function () {
+    $colorManager = new ColorStringHttp();
+    $sut = new ErrorPrinter($colorManager);
+
+    $result = $sut->printError('Failed asserting that 5 matches expected 4.');
+
+    expect($result)->toBe('Failed asserting that <fg=red;options=bold>5</> matches expected <fg=green;options=bold>4</>.');
 });
 
 test('文字列が返る(そのまま, foo)', function () {
